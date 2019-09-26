34337a7b9bf3cd5d0d7d73e17c6eded67f2fb83f
Darío <dariiodallara@gmail.com>
2019-09-26 14:37:51 -0300

    Agrego proyecto Entidades y creo clase Persona

---
+++ Entidades/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Persona
+    {
+        string nombre, apellido, dni;
+        int id;
+        DateTime fechaNacimiento;
+        public Persona()
+        {
+
+        }
+        public int Edad()
+        {
+            return 0;
+        }
+        public override string ToString()
+        {
+            return nombre + " " + apellido + " " + dni;
+        }
+    }
+}
